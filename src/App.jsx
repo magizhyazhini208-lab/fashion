6e44c44e7944f8c38f36f94add2d16eeee74745e
magizhyazhini208-lab <magizhyazhini208@gmail.com>
2025-11-23 03:18:21 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Login from './login'
+import Navbar from './navbar'
+import ProductList  from './productList'
+import AutoScrollImages from './autoScrollImages'
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <AutoScrollImages/>
+    <ProductList/>
+    </>
+  )
+}
+export default App
---
+++ src/navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar()
+{
+    return(
+        <div className="Navbar">
+            <h1>Welcome To Forever Fashion!!!!</h1>
+        </div>
+    );
+}
+export default Navbar
