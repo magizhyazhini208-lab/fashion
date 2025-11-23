f8d447c71b47a96e1000bb8b6abf7bf218840bc3
magizhYazhini <magizhyazhini208@gmail.com>
2025-11-23 16:47:05 +0530

    Updated responsive CSS and components

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,9 +1,25 @@
+import { useState, useEffect } from "react";
 function Navbar()
 {
-    return(
-        <div className="Navbar">
-            <h1>Welcome To Forever Fashion!!!!</h1>
+    const colors= ["red", "blue", "green", "brown"];
+        const [index, setIndex] = useState(0);
+    
+        useEffect(() => {
+            const interval = setInterval(() => {
+                setIndex(prev => (prev + 1) % colors.length);
+            }, 1000); // 10 seconds
+    
+            return () => clearInterval(interval);
+        }, []);
+    
+        return(
+        <div className="navbar">
+            <input type="text" name="searchbar"  placeholder="🔎search" className='display'/>
+            <h1>Welcome To Forever Fashion!!!!{colors.index}</h1> 
+            <h5 className="navbar1" style={{ color: colors[index], transition: "color 0.5s ease" }}>Sale is live now!!!!{colors.index}</h5> 
         </div>
+    
     );
+    
 }
 export default Navbar
